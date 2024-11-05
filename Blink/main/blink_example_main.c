1fbf0872a4db8071c594b0ed47c39a3f09187be9
Syed Nizamuddin Peeran <r.syed.nizamuddin.peeran@gmail.com>
2024-11-05 22:35:39 +0530

    removed components not required to make the led blink

--- Blink/main/blink_example_main.c
+++ Blink/main/blink_example_main.c
@@ -1,46 +1,29 @@
 #include "freertos/FreeRTOS.h"
+// to use vTaskDelay,portTICK_PERIOD_MS
 #include "driver/gpio.h"
-#include "esp_log.h"
-#include "led_strip.h"
-#include "sdkconfig.h"
-
-static const char *TAG = "BLINKExample";
-
-/* Use project configuration menu (idf.py menuConfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+//to use gpio_set_level,gpio_reset_pin,gpio_set_direction,GPIO_MODE_OUTPUT
 
 static uint8_t s_led_state = 0;
+static const uint8_t LED_GPIO = 2;
+static const int DELAY = (int)1000;
 
-#ifdef CONFIG_BLINK_LED_GPIO
-
-static void blink_led(void)
+static void flip_led_state(void)
 {
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
+    s_led_state = !s_led_state;
+    gpio_set_level(LED_GPIO, s_led_state);
 }
 
 static void configure_led(void)
 {
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
 }
-
-#else
-#error "unsupported LED type"
-#endif
 
 void app_main(void)
 {
     configure_led();
     while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        flip_led_state();
+        vTaskDelay(DELAY / portTICK_PERIOD_MS);
     }
 }
