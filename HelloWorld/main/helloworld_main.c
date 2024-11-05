08498f7d6ed372c38f209a39e1b8d4d23eeee26a
Syed Nizamuddin Peeran <r.syed.nizamuddin.peeran@gmail.com>
2024-11-05 22:44:38 +0530

    simplified hello world further

--- HelloWorld/main/helloworld_main.c
+++ HelloWorld/main/helloworld_main.c
@@ -1,10 +1,6 @@
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-
-static const char *TAG = "HelloWorldExample";
-
+#include <stdio.h>
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "Hello %s!", "World");
+    printf("Hello %s!", "World");
 }
